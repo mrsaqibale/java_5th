b08c3cd51bbe2244be396db23017f2d00942042a
codewithprofessor <kalikali72547254@gmail.com>
2023-09-25 22:17:29 +0500

    week01 day 01 change 2

---
+++ 01week/03_sum_code.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] argu){
+        //set the values
+        int num1 = 30;
+        int num2 = 20;
+
+        //add the both number
+        int sum = num1 + num2;
+
+        //print the sum using print method
+        System.out.print("Sum = " + sum + "\n");
+    }
+    
+}
