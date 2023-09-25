425fbe3a31898b59b05bcf435b3202a0daf4f6cb
codewithprofessor <kalikali72547254@gmail.com>
2023-09-25 22:12:17 +0500

    week01 day 01

---
+++ 01week/01_Hello_code.java
@@ -0,0 +1,6 @@
+//code by saqib ale
+class Main{
+    public static void main(String[] arg){
+        System.out.print("hello code");
+    }
+}
---
+++ 01week/02_Print_new_line.java
@@ -0,0 +1,8 @@
+//code by saqib ale
+// write java code print code on new line with 2 methods
+class Main{
+    public static void main(String[] argu){
+        System.out.print("hello code \n");  //method 01 print new line
+        System.out.println("hello code 1");   //method 02 print new line
+    }
+}
