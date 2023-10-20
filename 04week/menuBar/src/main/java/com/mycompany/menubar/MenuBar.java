ca6fee89cc131d5864d75509c641ef051eb35974
codewithprofessor <kalikali72547254@gmail.com>
2023-10-21 00:13:03 +0500

    add week 4 some code

---
+++ 04week/menuBar/src/main/java/com/mycompany/menubar/MenuBar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menubar;
+
+/**
+ *
+ * @author professor_ali
+ */
+public class MenuBar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
