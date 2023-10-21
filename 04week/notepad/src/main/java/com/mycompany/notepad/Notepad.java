7ffe0b35853313525a4f03e8ca3c47dd7a5b5cfa
codewithprofessor <kalikali72547254@gmail.com>
2023-10-21 23:18:37 +0500

    add notepad code

---
+++ 04week/notepad/src/main/java/com/mycompany/notepad/Notepad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.notepad;
+
+/**
+ *
+ * @author professor_ali
+ */
+public class Notepad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
