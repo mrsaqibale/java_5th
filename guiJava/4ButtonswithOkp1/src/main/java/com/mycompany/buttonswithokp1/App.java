f7e7172264b49e0490551802713c0ff34ce8c548
codewithprofessor <kalikali72547254@gmail.com>
2023-11-17 03:05:33 +0500

    add in practice set

---
+++ guiJava/4ButtonswithOkp1/src/main/java/com/mycompany/buttonswithokp1/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.buttonswithokp1;
+
+/**
+ *
+ * @author professor_ali
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
