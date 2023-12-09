2fecbdaf686aac71391a3485c4208955645a97e0
codewithprofessor <kalikali72547254@gmail.com>
2023-12-09 23:35:09 +0500

    add jdbc

---
+++ guiJava/loginAndRegisterWithFile/src/main/java/com/mycompany/loginandregisterwithfile/LoginAndRegisterWithFile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginandregisterwithfile;
+
+/**
+ *
+ * @author professor_ali
+ */
+public class LoginAndRegisterWithFile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
